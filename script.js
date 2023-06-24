d4410683c98e3cea8297fa8709fc41967e395ea3
avinash <avinashavi0702@gmail.com>
2023-06-24 12:39:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// const countersEl = document.querySelectorAll(".counter");
+
+// countersEl.forEach((counterEl) => {
+//   counterEl.innerText = "0";
+//   incrementCounter();
+//   function incrementCounter() {
+//     let currentNum = +counterEl.innerText;
+//     const dataCeil = counterEl.getAttribute("data-ceil");
+//     const increment = dataCeil / 15;
+//     currentNum = Math.ceil(currentNum + increment);
+
+//     if (currentNum < dataCeil) {
+//       counterEl.innerText = currentNum;
+//       setTimeout(incrementCounter, 50);
+//     } else {
+//       counterEl.innerText = dataCeil;
+//     }
+//   }
+// });
+
+
+
+const counterEl =document.querySelectorAll(".counter")
+
+counterEl.forEach((counterEl) => {
+    counterEl.innerText = "0"
+incrementCounter()
+    function incrementCounter(){
+        let currentNum = +counterEl.innerText
+        const dataCeil = counterEl.getAttribute("data-ceil")
+        const increment = dataCeil / 15
+        currentNum = Math.ceil(currentNum + increment)
+
+        if(currentNum < dataCeil){
+            counterEl.innerText = currentNum
+            setTimeout(incrementCounter, 50)
+        } else {
+            counterEl.innerText = dataCeil
+        }
+    }
+})
